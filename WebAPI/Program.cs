9ebc6f42979d74ca9bcb662e5bb124211481ddfd
Melih DİNÇER <m_dincer41@hotmail.com>
2024-02-14 15:18:41 +0300

    14 Şubat 2.Ders sonu.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,11 @@
+using DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDataAccessServices(builder.Configuration);
 
 var app = builder.Build();
 
