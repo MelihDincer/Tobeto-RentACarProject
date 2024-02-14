3d4cc533a329f73955cf5b13425013ca97376ba3
Melih DİNÇER <m_dincer41@hotmail.com>
2024-02-14 14:09:09 +0300

    Add project files.

---
+++ Entities/Concretes/Car.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities;
+
+namespace Entities.Concretes
+{
+    public class Car : BaseEntity<int>
+    {
+        public int ModelId{ get; set; }
+        public int ModelYear { get; set; }
+        public string Plate { get; set; }
+        public int State { get; set; } // 1-Avaible 2-Rented 3-Under Maitenance
+        public double DailyPrice { get; set; }
+
+        public Model Model { get; set; }
+
+        public Car()
+        {
+            
+        }
+
+        public Car(int modelId, int modelYear, string plate, int state, double dailyPrice, Model model)
+        {
+            Id = modelId;
+            ModelId = modelId;
+            ModelYear = modelYear;
+            Plate = plate;
+            State = state;
+            DailyPrice = dailyPrice;
+        }
+    }
+}
