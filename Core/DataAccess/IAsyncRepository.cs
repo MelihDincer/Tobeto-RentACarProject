55276896cba04edf3282d34ea755f0ba68b5f678
Melih DİNÇER <m_dincer41@hotmail.com>
2024-02-16 14:02:48 +0300

    16 Şubat 1.Ders sonu.

--- Core/DataAccess/IAsyncRepository.cs
+++ Core/DataAccess/IAsyncRepository.cs
@@ -4,14 +4,19 @@ using System.Linq.Expressions;
 
 namespace Core.DataAccess;
 
+//IAsyncRepository => arayüz 
+//IQuery => IQueryable türünde bir metodun imzasını tanımladığımız bir interface.
+//Interface interface'i extend eder. Implement etmez.
 public interface IAsyncRepository<TEntity, TEntityId> : IQuery<TEntity>
     where TEntity : BaseEntity<TEntityId>
-
 {
+    //Modeldeki ilişkili tablolarda join işlemi için IIncludableQueryable interface ini kullandık.
+    //Task asenkron yapının kullanılabilmesine olanak sağlar.
+    //predicate = null   :> filtreleme işlemi null olabilir demek istedik. GetAll da kullandık çünkü orada bir şart olmayabilir tüm verileri listeleyebiliriz. Aşağıdaki get metodunda kullanmamamızın sebebi her türlü bir şartın geçmesi durumu olduğu için. Getirilecek veri bir şarta göre bir tane olacak şekilde gelecek sonuçta.(Çünkü List değil)
     Task<List<TEntity>> GetAll(Expression<Func<TEntity, bool>> predicate = null,
-        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null);
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null);//Gelen veriler null olabilir diye "?" kullandık.
     Task<TEntity> Get(Expression<Func<TEntity, bool>> predicate,
-        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null);
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null); //Gelen veriler null olabilir diye "?" kullandık.
 
     Task<TEntity> Add(TEntity entity);
     Task<TEntity> Update(TEntity entity);
--- Core/DataAccess/IQuery.cs
+++ Core/DataAccess/IQuery.cs
@@ -1,5 +1,8 @@
 ﻿namespace Core.DataAccess;
+
+//
 public interface IQuery<T>
 {
+    //IQueryable, entityframeworkde kullanabildiğimiz daha güzel sorgular yazabildiğimiz bir interface.
     IQueryable<T> Query();
 }
